3b9fdd33871caa6705b0ac95bbdc3fc29ed3edbc
Mark Sinclair <mark.sinclair@healthforge.io>
2016-11-03 10:12:16 +0000

    Fixes

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ if [ ! -f /etc/network/interfaces.d/100-openvpn-jenkins.cfg ]; then
     sudo echo $'iface jenkins-vpn inet auto\nopenvpn jenkins' > /etc/network/interfaces.d/100-openvpn-jenkins.cfg
 fi
 
-if [ ! -f /etc/openvpn/jenkins.cfg ]; then
+if [ ! -f /etc/openvpn/jenkins.conf ]; then
     sudo cp $1 /etc/openvpn/jenkins.conf
 fi
 
